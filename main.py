226dace154101c4c39501e4da05147eba58a0d10
MinJune Kim <qria@qria.net>
2025-08-28 16:16:09 -0700

    Support Pensieve

--- main.py
+++ main.py
@@ -2,10 +2,12 @@ import json
 
 import src.handle_email_queue as handle_email
 import src.handle_flush_gradescope as handle_flush
+import src.handle_flush_pensieve as handle_flush_pensieve_module
 import src.handle_form_submit as handle_form
+from src.environment import Environment
 from src.errors import KnownError
 from src.slack import SlackManager
-from src.utils import Environment, truncate
+from src.utils import truncate
 
 
 def handle_email_queue(request):
@@ -31,6 +33,24 @@ def handle_flush_gradescope(request):
     print("handle_flush_gradescope called on payload: " + json.dumps(request_json))
     try:
         handle_flush.handle_flush_gradescope(request_json=request_json)
+        return {"success": True}
+    except KnownError as e:
+        print("Known Error Occurred: " + str(e) + f" (Request: {request_json})")
+        SlackManager().send_error(str(e) + f" (Request: {truncate(request_json)})")
+        return {"success": False, "error": str(e)}
+    except Exception as e:
+        print("Internal Error Occurred: " + str(e) + f" (Request: {request_json})")
+        SlackManager().send_error("Internal error: " + str(e) + f" (Request: {truncate(request_json)})")
+        raise
+    finally:
+        Environment.clear()
+
+
+def handle_flush_pensieve(request):
+    request_json = request.get_json()
+    print("handle_flush_pensieve called on payload: " + json.dumps(request_json))
+    try:
+        handle_flush_pensieve_module.handle_flush_pensieve(request_json=request_json)
         return {"success": True}
     except KnownError as e:
         print("Known Error Occurred: " + str(e) + f" (Request: {request_json})")
